5911a3224f24115c572db72b1688b48229a413aa
Roma <ranex@yandex.ru>
2019-07-03 17:15:36 +1000

    new commit

--- app/src/main/java/com/demo/braintrainer/MainActivity.java
+++ app/src/main/java/com/demo/braintrainer/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTimer;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewOpinion1;
     private TextView textViewOpinion2;
     private TextView textViewOpinion3;
+    private ArrayList<TextView> options = new ArrayList<>();
 
     private String question;
     private int rightAnswer;
@@ -35,14 +38,47 @@ public class MainActivity extends AppCompatActivity {
         textViewOpinion1 = findViewById(R.id.textViewOpinion1);
         textViewOpinion2 = findViewById(R.id.textViewOpinion2);
         textViewOpinion3 = findViewById(R.id.textViewOpinion3);
+        options.add(textViewOpinion0);
+        options.add(textViewOpinion1);
+        options.add(textViewOpinion2);
+        options.add(textViewOpinion3);
+
+        generateQuestions();
+        for (int i = 0; i<options.size(); i++){
+            if(i == rightAnswerPosition){
+                options.get(i).setText(Integer.toString(rightAnswer));
+            }
+            else {
+                options.get(i).setText(Integer.toString(generateWrongAnswer()));
+            }
+        }
 
     }
 
-    private void generateQuestions()
-    {
+    private void generateQuestions(){
         int a = (int)(Math.random() * (max-min+1) + min);
         int b = (int)(Math.random() * (max-min+1) + min);
         int mark = (int)(Math.random() * 2);
         isPositive = mark == 1;
+
+        if(isPositive){
+            rightAnswer = a + b;
+            question = String.format("%s + %s", a,b);
+        }
+        else{
+            rightAnswer = a - b;
+            question = String.format("%s - %s", a,b);
+        }
+        rightAnswerPosition = (int) Math.random()*4;
+        //присваеваем полю сгенерированый вопросс
+        textViewQuestion.setText(question);
+    }
+
+    private int generateWrongAnswer(){
+        int result;
+        do{
+            result = (int) (Math.random() * max * 2 + 1)-(max-min);
+        }while (result == rightAnswer);
+        return result;
     }
 }
